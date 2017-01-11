89ef956ce11155b44c2dca62d13549c6ca2179f5
zhangwuyang <zhangwuyang@126.com>
2017-01-11 20:52:51 +0800

    提交4

--- src/GitTest.java
+++ src/GitTest.java
@@ -1,6 +1,12 @@
 
 public class GitTest {
 	public static void main(String[] args) {
-		System.out.println("123");
+		System.out.println("1");
+		System.out.println("1");
+		System.out.println("1");
+		System.out.println("1");
+		System.out.println("1");
+		System.out.println("1");
+		System.out.println("1");
 	}
 }
